135b47003670f129129765a4f90225742a1c9204
Ewout van der Veer <evanderveer@users.noreply.github.com>
2023-10-20 09:03:47 +0200

    Using Enums in LCR class

--- nfoinstruments/Interfaces/LCR.py
+++ nfoinstruments/Interfaces/LCR.py
@@ -1,26 +1,54 @@
 from pprint import pprint
+from enum import Enum, auto
+
+class SignalType(Enum):
+    CURRENT = auto()
+    VOLTAGE = auto()
+
+class MeasurementTime(Enum):
+    SHORT = 'SHOR'
+    MEDIUM = 'MED'
+    LONG = 'LONG'
+
+class MeasurementType(Enum):
+    CPD = 'CPD'
+    CPQ = 'CPQ'
+    CPG = 'CPG'
+    CPRP = 'CPRP'
+    CSD = 'CSD'
+    CSQ = 'CSQ'
+    CSRS = 'CSRS'
+    LPD = 'LPD'
+    LPQ = 'LPQ'
+    LPG = 'LPG'
+    LPRD = 'LPRD'
+    LSD = 'LSD'
+    LSQ = 'LSQ'
+    LSRD = 'LSRD'
+    LSRS = 'LSRS'
+    RX = 'RX'
+    ZTD = 'ZTD'
+    ZTR = 'ZTR'
+    GB = 'GB'
+    YTD = 'YTD'
+    YTR = 'YTR'
+    VDID = 'VDID'
 
 class LCR:
-
-    valid_measurement_types = ["CPD","CPQ","CPG","CPRP","CSD","CSQ","CSRS","LPD","LPQ",
-                               "LPG","LPRP","LPRD","LSD","LSQ","LSRS","LSRD","RX","ZTD",
-                               "ZTR","GB","YTD","YTR","VDID"]
-    #srq = pyvisa.constants.EventType.service_request
-    #srq_queue = pyvisa.constants.EventMechanism.queue
 
     def __init__(self, address, resman):
         self.address = address
         self.resource = resman.open_resource(self.address, query_delay=0.1)
 
-        self._measurement_time = 'MED'
+        self._measurement_time = MeasurementTime.MEDIUM
         self._averages = 1
         self._bias = 0
         self._frequency = 100
-        self._measurement_type = 'CPD'
+        self._measurement_type = MeasurementType.RX
         self._signal_amplitude = 1
         self.measurement_timeout = 3
         self.instrument_name = 'Agilent E4980A'
-        self._signal_type = 'voltage'
+        self._signal_type = SignalType.VOLTAGE
         self._alc_enabled = True
 
         self._initialize()
@@ -29,13 +57,14 @@ class LCR:
     def _initialize(self):
         self.resource.clear()
         self.resource.write('*RST')
-        self.resource.write(f"APER {self._measurement_time}, {self._averages}")
+        self.resource.write(f"APER {self._measurement_time.value}, 
+                                   {self._averages}")
         self.resource.write("BIAS:STAT OFF")
         self.resource.write(f"BIAS:VOLT {self._bias}")
         
         self.resource.write(f"FREQ {self._frequency}")
         self.resource.write(f"VOLT {self._signal_amplitude}")
-        self.resource.write(f"FUNC:IMP:TYPE {self._measurement_type}")
+        self.resource.write(f"FUNC:IMP:TYPE {self._measurement_type.value}")
         self.resource.write("INIT:CONT ON")
         self.resource.write("AMPL:ALC ON")
         self.resource.write("FORMAT ASCII")
@@ -62,10 +91,10 @@ class LCR:
 
     @measurement_time.setter
     def measurement_time(self, time):
-        if not time in ['SHORT', 'SHOR', 'MED', 'MEDIUM', 'LONG']:
+        if not isinstance(time, MeasurementTime):
             raise ValueError("measurement time must be SHORT, MEDIUM or LONG")
         self._measurement_time = time
-        self.resource.write(f"APER {self._measurement_time}, {self._averages}")
+        self.resource.write(f"APER {self._measurement_time.value}, {self._averages}")
     
     @property
     def averages(self):
@@ -76,7 +105,7 @@ class LCR:
         if not 1 <= averages <= 256:
             raise ValueError("number of averages must be between 1 and 256")
         self._averages = averages
-        self.resource.write(f"APER {self._measurement_time}, {self._averages}")
+        self.resource.write(f"APER {self._measurement_time.value}, {self._averages}")
 
     @property
     def bias(self):
@@ -84,7 +113,7 @@ class LCR:
 
     @bias.setter
     def bias(self, bias):
-        if self.signal_type == 'voltage':
+        if self.signal_type == SignalType.VOLTAGE:
             if not -40 <= bias <= 40:
                 raise ValueError("bias must be between -40 and 40 V")
             self._bias = bias
@@ -123,10 +152,10 @@ class LCR:
 
     @measurement_type.setter
     def measurement_type(self, measurement_type):
-        if not measurement_type in LCR.valid_measurement_types:
+        if not isinstance(measurement_type, MeasurementType):
             raise ValueError("measurement type invalid")
         self._measurement_type = measurement_type
-        self.resource.write(f"FUNC:IMP:TYPE {self._measurement_type}")
+        self.resource.write(f"FUNC:IMP:TYPE {self._measurement_type.value}")
 
     @property
     def signal_amplitude(self):
@@ -134,12 +163,12 @@ class LCR:
 
     @signal_amplitude.setter
     def signal_amplitude(self, signal_amplitude):
-        if self.signal_type == 'voltage':
+        if self.signal_type == SignalType.VOLTAGE:
             if not 0 <= signal_amplitude <= 20:
                 raise ValueError("voltage signal amplitude must be between 0 and 20 V")
             self._signal_amplitude = signal_amplitude
             self.resource.write(f"VOLT {self._signal_amplitude}")
-        elif self.signal_type == 'current':
+        elif self.signal_type == SignalType.CURRENT:
             if not 0 <= signal_amplitude <= 0.1:
                 raise ValueError("current signal amplitude must be between 0 and 0.1 A")
             self._signal_amplitude = signal_amplitude
@@ -151,7 +180,7 @@ class LCR:
 
     @signal_type.setter
     def signal_type(self, signal_type):
-        if not signal_type in ['voltage', 'current']:
+        if not isinstance(signal_type, SignalType):
             raise ValueError("signal type must be 'voltage' or 'current")
         self._signal_type = signal_type
         self.resource.write(f"CURR 0")
